1e20081282816609930cc85a7d6ca2edb278afa5
AlexandreLuercio <alexandreluercio@gmail.com>
2025-12-02 16:29:02 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  // Configuração simplificada e robusta para Vercel
   const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react()],
