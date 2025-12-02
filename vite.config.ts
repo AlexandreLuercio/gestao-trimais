785525b39794fe485b8484fdfc59a85889a851a1
AlexandreLuercio <alexandreluercio@gmail.com>
2025-12-02 17:02:11 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  // Casting process to any to avoid strict type checking issues on build environments
   const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react()],
