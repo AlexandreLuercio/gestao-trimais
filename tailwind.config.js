e457140d878bf6a85f1b557b8004f6c818933a67
AlexandreLuercio <alexandreluercio@gmail.com>
2025-12-15 17:07:05 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-content: [
-"./index.html
-"./src/**/*.{js,ts,jsx,tsx}",
-],
-theme: {
-extend: {},
-},
-plugins: [],
+  content: [
+    "./index.html", // <-- ADICIONE A VÍRGULA AQUI!
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
